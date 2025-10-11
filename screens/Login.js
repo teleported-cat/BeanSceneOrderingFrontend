1a65aec735df9ec8724c52348e3d410030142fa1
Pat <patrickross216@gmail.com>
2025-10-11 23:44:44 +1100

    Adds login screen

---
+++ screens/Login.js
@@ -0,0 +1,34 @@
+{/* Components */}
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+{/* Stylesheet */}
+import Style from '../styles/Style.js';
+
+export default function Login() {
+    return (
+        <SafeAreaView style={[Style.center, Style.loginBackground]}>
+            <ScrollView contentContainerStyle={Style.center}>
+                {/* Logo Box */}
+                <View style={[Style.loginLogoBox, Style.center]}>
+                    <Image style={Style.loginImage} source={require('../assets/brand_materials/logo/png/logo-secondary-transparent.png')} resizeMode='contain'></Image>
+                    <Text style={[Style.loginTitle, Style.boldText]}>Ordering System</Text>
+                </View>
+
+                {/* Message Box */}
+                <View style={Style.loginMessageBox}>
+                    <Text style={[Style.loginMessageText, Style.italicText]}>Test Message!</Text>
+                </View>
+
+                {/* Form Box */}
+                <View style={[Style.loginFormBox, Style.topCenter]}>
+                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Username'></TextInput>
+                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Password'></TextInput>
+                    <TouchableOpacity style={[Style.loginButton]}>
+                        <Text style={[Style.loginButtonText, Style.boldText]}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
---
+++ styles/Style.js
@@ -0,0 +1,97 @@
+import { StyleSheet } from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+//#region Palette
+const white = '#fff';
+const black = '#000';
+const darkBlue = '#083944';
+const midBlue = '#2F6672';
+const lightBlue = '#4AA1B5';
+const gold = '#EBC136';
+const paleGold = '#F8E8B5';
+const darkGold = '#CC9E09';
+const lightGrey = '#E0E0E0';
+const darkGrey = '#808080';
+const danger = '#FF3030';
+//#endregion
+//#region Constants
+const cLoginBackground = darkBlue;
+const cTextInputBorder = lightBlue;
+const cButtonBackground = lightBlue;
+//#endregion
+
+export default StyleSheet.create({
+    //#region General
+    row: {
+        flexDirection: 'row',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    topCenter: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    regularText: {
+        fontFamily: 'OpenSans_400Regular',
+    },
+    italicText: {
+        fontFamily: 'OpenSans_400Regular_Italic',
+    },
+    boldText: {
+        fontFamily: 'OpenSans_700Bold',
+    },
+    //#endregion
+    //#region Login
+    loginBackground: {
+        backgroundColor: cLoginBackground,
+    },
+    loginLogoBox: {
+        gap: 10,
+    },
+    loginImage: {
+        width: wp('100%'),
+        maxWidth: 412,
+        maxHeight: 149,
+    },
+    loginTitle: {
+        fontSize: 32,
+        color: white,
+    },
+    loginMessageBox: {
+        paddingBottom: 10,
+    },
+    loginMessageText: {
+        color: danger,
+        fontSize: 24,
+    },
+    loginFormBox: {
+        width: wp('80%'),
+        gap: 12,
+    },
+    loginInput: {
+        width: wp('80%'),
+        backgroundColor: white,
+        borderColor: cTextInputBorder,
+        borderWidth: 3,
+        borderRadius: 5,
+        padding: 10,
+        fontSize: 24,
+        color: darkGrey,
+    },
+    loginButton: {
+        width: wp('80%'),
+        backgroundColor: cButtonBackground,
+        padding: 10,
+        borderRadius: 5,
+    },
+    loginButtonText: {
+        fontSize: 24,
+        color: white,
+        textAlign: 'center',
+    },
+    //#endregion
+});
