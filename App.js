c8b4c10d311958bfcb4ad3c852c39659de50958c
Pat <patrickross216@gmail.com>
2025-10-11 23:44:18 +1100

    Updates App.js to load custom fonts

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
+{/* Components */}
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts, OpenSans_400Regular, OpenSans_400Regular_Italic, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+
+{/* Navigation */}
+
+
+{/* Screens */}
+import Login from './screens/Login.js';
+
+{/* Styles */}
+import Style from './styles/Style.js';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_400Regular_Italic,
+    OpenSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Login/>
+    </SafeAreaProvider>
   );
 }
 
