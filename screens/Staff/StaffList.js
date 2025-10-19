30a245c4589290a70718ecb882036cea908486a4
Pat <patrickross216@gmail.com>
2025-10-19 18:32:02 +1100

    Adds staff deletion to staff list

--- screens/Staff/StaffList.js
+++ screens/Staff/StaffList.js
@@ -11,6 +11,7 @@ import { API_BASE_URL } from '../../components/APIAddress.js';
 import Style from '../../styles/Style.js';
 
 export default function StaffList({props, navigation}) {
+    //#region GET Staff
     const [staffData, setStaffData] = useState([]);
 
     const isFocused = useIsFocused();
@@ -37,11 +38,62 @@ export default function StaffList({props, navigation}) {
             getStaff();
         }
     }, [props, isFocused])
+    //#endregion
+    //#region DELETE methods
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedStaff, setSelectedStaff] = useState({});
 
+    const showModal = (staff) => {
+        setSelectedStaff(staff);
+        setModalVisible(true);
+    };
+
+    const hideModal = () => {
+        setModalVisible(false);
+        setSelectedStaff({});
+    };
+
+    const confirmDelete = async () => {
+        var url = `${API_BASE_URL}/Staff/${selectedStaff._id}`;
+        var header = new Headers({});
+        header.append('Content-Type', "application/json");
+        var options = {
+            method: "DELETE",
+            headers: header
+        };
+
+        try {
+            const response = await fetch(url, options);
+            console.log("Staff deleted!");
+            hideModal();
+            getStaff();
+        } catch(error) {
+            console.log("DELETE Staff failed: " + error.message);
+        }
+    };
+    //#endregion
     return(
         <SafeAreaView style={[Style.center, Style.background]}>
             {/* Auth Header */}
             <AuthHeader/>
+
+            {/* Item Delete Modal */}
+            <Modal visible={modalVisible} transparent>
+                <View style={Style.modalBox}>
+                    <View style={Style.modal}>
+                        <Text style={[Style.modalMessage, Style.regularText]}>Are you sure you want to delete {selectedStaff.firstName} {selectedStaff.lastName}?</Text>
+                        <View style={Style.modalActions}>
+                            <TouchableOpacity style={[Style.modalButton, Style.modalCancel]} onPress={() => hideModal()}>
+                                <Text style={[Style.modalButtonText, Style.regularText]}>Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={[Style.modalButton, Style.modalDelete]} onPress={() => confirmDelete()}>
+                                <Text style={[Style.modalButtonText, Style.regularText]}>Delete</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+            
             <ScrollView contentContainerStyle={[Style.topCenter, Style.scrollView]}>
                 <View style={Style.pageContent}>
                     {/* Item Header */}
@@ -64,7 +116,7 @@ export default function StaffList({props, navigation}) {
                                         <TouchableOpacity style={[Style.actionButton, Style.actionEdit]} onPress={() => navigation.navigate('Update Staff', {item})}>
                                             <Ionicons name='pencil-outline' size={20} color='white'></Ionicons>
                                         </TouchableOpacity>
-                                        <TouchableOpacity style={[Style.actionButton, Style.actionDelete]} >
+                                        <TouchableOpacity style={[Style.actionButton, Style.actionDelete]} onPress={() => showModal(item)}>
                                             <Ionicons name='trash-outline' size={20} color='white'></Ionicons>
                                         </TouchableOpacity>
                                     </View>
