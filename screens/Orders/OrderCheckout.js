07f17a8db781843e02401f300fe25943a498adbe
Pat <patrickross216@gmail.com>
2025-10-21 22:36:34 +1100

    Partially adds UI to order checkout

--- screens/Orders/OrderCheckout.js
+++ screens/Orders/OrderCheckout.js
@@ -1,19 +1,54 @@
 {/* Components */}
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity, Modal, Picker } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 import AuthHeader from '../../components/AuthHeader.js';
+import { API_BASE_URL } from '../../components/APIAddress.js';
+import ImageFallback from '../../components/ImageFallback.js';
 
 {/* Stylesheet */}
 import Style from '../../styles/Style.js';
 
-export default function OrderCheckout() {
+export default function OrderCheckout({props, route, navigation}) {
     return(
         <SafeAreaView style={[Style.center, Style.background]}>
             {/* Auth Header */}
             <AuthHeader/>
             <ScrollView contentContainerStyle={[Style.topCenter, Style.scrollView]}>
                 <View style={Style.pageContent}>
-                    <Text>OrderCheckout</Text>
+                    {/* Cancel Order Button */}
+                    <View style={Style.backButtonBox}>
+                        <TouchableOpacity style={Style.backButton} onPress={() => navigation.navigate('Order New')}>
+                            <Ionicons name='arrow-back-outline' size={24} color='white'></Ionicons>
+                            <Text style={[Style.backButtonText, Style.boldText]}>Back to Items</Text>
+                        </TouchableOpacity>
+                        <View style={Style.invisibleFill}></View>
+                    </View>
+
+                    {/* Checkout Header */}
+                    <View style={Style.listHeader}>
+                        <Text style={[Style.listHeaderText, Style.boldText]}>Checkout</Text>
+                    </View>
+
+                    {/* Details Header */}
+                    <View style={Style.listHeader}>
+                        <Text style={[Style.listHeaderText, Style.boldText]}>Details</Text>
+                    </View>
+
+                    {/* Details Form */}
+                    <View>
+
+                    </View>
+
+                    {/* Form Button */}
+                    <View style={Style.formButtonBox}>
+                        <TouchableOpacity style={Style.formButton}>
+                            <Text style={[Style.formButtonText, Style.boldText]}>Place Order</Text>
+                            <Ionicons name='checkmark-circle-outline' size={24} color='white'></Ionicons>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
