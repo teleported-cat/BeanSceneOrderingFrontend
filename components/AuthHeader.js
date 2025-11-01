7c73fa520da704326166ac394edfa82514559e30
Pat <patrickross216@gmail.com>
2025-11-01 18:08:55 +1100

    Adds login functionality for login screen and saving user details for auth header

--- components/AuthHeader.js
+++ components/AuthHeader.js
@@ -1,29 +1,61 @@
 {/* Components */}
+import { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Ionicons } from '@expo/vector-icons';
 
 {/* Styles */}
 import Style from '../styles/Style';
 
-export default function AuthHeader() {
+export default function AuthHeader({props}) {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [role, setRole] = useState('');
     
     const navigation = useNavigation();
+
+    const isFocused = useIsFocused();
 
     const logoutUser = () => {
         navigation.navigate('Login');
     };
 
+    const retrieveUserData = async () => {
+        let userObject;
+        console.log("Getting User for Auth")
+        try {
+            const user = await AsyncStorage.getItem('CurrentUser');
+            userObject = JSON.parse(user);
+            console.log(user);
+        } catch (error) {
+            console.log("Auth Async-Storage Error: " + error);
+            return;
+        }
+
+        console.log('Setting useStates')
+        setFirstName(userObject.firstName);
+        setLastName(userObject.lastName);
+        setRole(userObject.role);
+    };
+
+    useEffect(() => {
+        if (isFocused) {
+            retrieveUserData();
+        }
+    }, [props, isFocused]);
+
     return (
         <View style={Style.authHeader}>
             <Text style={[Style.authText, Style.italicText]}>
-                Hi John Doe! 👋
+                Hi {firstName} {lastName}! 👋
             </Text>
             <Text style={[Style.authText, Style.italicText]}>
-                Role
+                {role}
             </Text>
-            <TouchableOpacity style={Style.authLogout} onPress={logoutUser}>
+            <TouchableOpacity style={Style.authLogout} onPress={() => logoutUser()}>
                 <Ionicons name='log-out-outline' size={16}></Ionicons>
                 <Text style={[Style.authLogoutText, Style.regularText]}>Logout</Text>
             </TouchableOpacity>
--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,24 @@
 {/* Components */}
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useIsFocused } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_BASE_URL } from '../components/APIAddress.js';
+import { isBlank } from '../components/PostValidationMethods.js';
 
 {/* Stylesheet */}
 import Style from '../styles/Style.js';
 
-export default function Login({route, navigation}) {
+export default function Login({props, route, navigation}) {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [message, setMessage] = useState('');
+
+    const isFocused = useIsFocused();
+
 
     const loginManager = () => {
         navigation.navigate('Manager Dashboard');
@@ -14,6 +27,88 @@ export default function Login({route, navigation}) {
     const loginStaff = () => {
         navigation.navigate('Staff Dashboard');
     };
+
+    const validate = async () => {
+        // Validate required fields
+        if (isBlank(username)) {
+            setMessage("Username is required.");
+            return;
+        }
+        if (isBlank(password)) {
+            setMessage("Password is required.");
+            return;
+        }
+
+        // Create credentials object
+        var credentials = {
+            username: username,
+            password: password
+        };
+
+        // Send request
+        var url = `${API_BASE_URL}/Staff/Login`;
+        var header = new Headers({});
+        header.append('Content-Type', "application/json");
+        var options = {
+            method: "POST",
+            headers: header,
+            body: JSON.stringify(credentials)
+        };
+
+        let data;
+
+        try {
+            const response = await fetch(url, options);
+            if (response.status == 401) {
+                console.log("Unauthorised!");
+                setMessage("Username or Password is incorrect.");
+                return;
+            }
+            data = await response.json();
+            // Add plain text password to data for API auth
+            data.password = password;
+        } catch(error) {
+            console.log("Login failed: " + error.message);
+            setMessage("Something went wrong, try again.");
+            return;
+        }
+
+        // Save user data to async storage
+        try {
+            await AsyncStorage.setItem('CurrentUser', JSON.stringify(data));
+            console.log('Stored user!');
+            console.log(data);
+        } catch (error) {
+            console.log("Async-Storage Error: " + error);
+            return;
+        }
+
+        // Navigate to appropriate dashboard
+        if (data.role == "Manager") {
+            navigation.navigate('Manager Dashboard');
+        } else {
+            navigation.navigate('Staff Dashboard');
+        }
+
+    };
+
+    // Blanks out user data in async storage when login screen is loaded
+    const clearUser = async () => {
+        console.log('Clearing user data...');
+        try {
+            await AsyncStorage.setItem('CurrentUser', {});
+            console.log('Cleared!');
+        } catch (error) {
+            console.log("Clearing error!: " + error);
+            return;
+        }
+    };
+
+    useEffect(() => {
+        if (isFocused) {
+            clearUser();
+        }
+    }, [props, isFocused]);
 
     return (
         <SafeAreaView style={[Style.center, Style.loginBackground]}>
@@ -26,18 +121,15 @@ export default function Login({route, navigation}) {
 
                 {/* Message Box */}
                 <View style={Style.loginMessageBox}>
-                    <Text style={[Style.loginMessageText, Style.italicText]}>Test Message!</Text>
+                    <Text style={[Style.loginMessageText, Style.italicText]}>{message}</Text>
                 </View>
 
                 {/* Form Box */}
                 <View style={[Style.loginFormBox, Style.topCenter]}>
-                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Username'></TextInput>
-                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Password'></TextInput>
-                    <TouchableOpacity style={[Style.loginButton]} onPress={loginManager}>
-                        <Text style={[Style.loginButtonText, Style.boldText]}>Login Manager</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[Style.loginButton]} onPress={loginStaff}>
-                        <Text style={[Style.loginButtonText, Style.boldText]}>Login Staff</Text>
+                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Username' value={username} onChangeText={(username) => setUsername(username)}></TextInput>
+                    <TextInput style={[Style.loginInput, Style.regularText]} placeholder='Password' value={password} onChangeText={(password) => setPassword(password)} secureTextEntry></TextInput>
+                    <TouchableOpacity style={[Style.loginButton]} onPress={() => validate()}>
+                        <Text style={[Style.loginButtonText, Style.boldText]}>Login</Text>
                     </TouchableOpacity>
                 </View>
             </ScrollView>
